ab59aefcd4fdce52bb38e93b32cce2d1ce4b620f
Robert Silve <robert@silve.net>
2021-12-05 19:32:56 +0100

    add test

---
+++ src/test/java/net/silve/codec/command/handler/EmptyHandlerTest.java
@@ -0,0 +1,25 @@
+package net.silve.codec.command.handler;
+
+import io.netty.handler.codec.smtp.SmtpCommand;
+import net.silve.codec.ConstantResponse;
+import net.silve.codec.DefaultSmtpRequest;
+import net.silve.codec.command.HandlerResult;
+import net.silve.codec.session.MessageSession;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EmptyHandlerTest {
+    @Test
+    void shouldHaveName() {
+        CharSequence name = new EmptyHandler().getName();
+        assertEquals(SmtpCommand.EMPTY.name(), name);
+    }
+
+    @Test
+    void shouldReturnResponse() {
+        HandlerResult handle = new EmptyHandler().handle(DefaultSmtpRequest.newInstance(SmtpCommand.HELO), MessageSession.newInstance());
+        assertEquals(ConstantResponse.RESPONSE_EMPTY, handle.getResponse());
+    }
+
+}
