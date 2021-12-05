5175364c759d9b9416fa20efa9ca1591eecd50cc
Robert Silve <robert@silve.net>
2021-12-05 09:57:05 +0100

    add test

--- src/test/java/net/silve/codec/command/RSETHandlerTest.java
+++ src/test/java/net/silve/codec/command/RSETHandlerTest.java
@@ -1,17 +1,18 @@
 package net.silve.codec.command;
 
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.smtp.SmtpCommand;
 import io.netty.handler.codec.smtp.SmtpResponse;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GlobalEventExecutor;
 import net.silve.codec.ConstantResponse;
+import net.silve.codec.DefaultSmtpRequest;
+import net.silve.codec.SmtpRequest;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class RSETHandlerTest {
 
@@ -22,11 +23,19 @@ class RSETHandlerTest {
     }
 
     @Test
-    void shouldExecute() throws InterruptedException, ExecutionException {
-        ChannelHandlerContext context = new EmbeddedChannel().pipeline().lastContext();
-        Future<SmtpResponse> execute = new RSETHandler().execute(null, context, null);
-        SmtpResponse smtpResponse = execute.await().get();
-        assertEquals(ConstantResponse.RESPONSE_RSET_OK, smtpResponse);
+    void shouldExecute() {
+        SimpleChannelInboundHandler<SmtpRequest> handler = new SimpleChannelInboundHandler<>() {
+            @Override
+            protected void channelRead0(ChannelHandlerContext ctx, SmtpRequest request) {
+                RSETHandler rsetHandler = new RSETHandler();
+                rsetHandler.execute(request, ctx, null).addListener(future -> ctx.writeAndFlush(future.get()).addListener(ChannelFutureListener.CLOSE));
+            }
+        };
+        EmbeddedChannel channel = new EmbeddedChannel(handler);
+        assertFalse(channel.writeInbound(DefaultSmtpRequest.newInstance(SmtpCommand.EHLO)));
+        channel.finish();
+        SmtpResponse outbound = channel.readOutbound();
+        assertEquals(ConstantResponse.RESPONSE_RSET_OK, outbound);
     }
 
 }
