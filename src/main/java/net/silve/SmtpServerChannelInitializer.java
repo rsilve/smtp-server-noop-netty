70143e5eddaffd224d05b7a10a372204479aaee5
Robert Silve <robert@silve.net>
2021-12-12 18:29:01 +0100

    apply format

--- src/main/java/net/silve/SmtpServerChannelInitializer.java
+++ src/main/java/net/silve/SmtpServerChannelInitializer.java
@@ -6,20 +6,16 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
-import net.silve.codec.SmtpRequestHandler;
 import net.silve.codec.SmtpRequestDecoder;
+import net.silve.codec.SmtpRequestHandler;
 import net.silve.codec.SmtpResponseEncoder;
-
-import java.util.concurrent.TimeUnit;
 
 class SmtpServerChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     private static final ByteBuf CRLF_DELIMITER = Unpooled.wrappedBuffer(new byte[]{13, 10});
 
     @Override
-    public void initChannel(SocketChannel ch) throws Exception {
+    public void initChannel(SocketChannel ch) {
         ch.pipeline()
                 //.addLast(new ReadTimeoutHandler(1, TimeUnit.SECONDS))
                 //.addLast(new WriteTimeoutHandler(10, TimeUnit.SECONDS))
